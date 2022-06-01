c7d4ae41e19efdca860783b9582c5ab5eb9e40fe
unknown <muhammadsaadz100@gmail.com>
2022-06-01 17:58:41 +0500

    Added ListTile

--- lib/chart.dart
+++ lib/chart.dart
@@ -10,7 +10,7 @@ class Chart extends StatelessWidget {
   List<Map<String, Object>> get groupedTxns {
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(
-        Duration(days: index),
+        Duration(days: 6 - index),
       );
       var totalSum = 0.0;
       for (var i = 0; i < recenttxns.length; i++) {
@@ -21,7 +21,7 @@ class Chart extends StatelessWidget {
         }
       }
       return {
-        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'day': DateFormat.E().format(weekDay).substring(0, 3),
         "amount": totalSum
       };
     });
--- lib/transactions_list.dart
+++ lib/transactions_list.dart
@@ -25,41 +25,64 @@ class TransactionList extends StatelessWidget {
           : ListView(
               children: transactions.map((tx) {
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        constraints: BoxConstraints(minWidth: 88, maxWidth: 88),
-                        padding: const EdgeInsets.all(10),
-                        margin: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Theme.of(context).colorScheme.primary,
-                          width: 2,
-                        )),
-                        child: Text(
-                          '\$' "${tx.amount.toStringAsFixed(2)}",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.black),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(tx.title,
-                              style: const TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold)),
-                          Text(DateFormat.yMMMMd("en_US").format(tx.date),
-                              style: const TextStyle(
-                                  color: Colors.grey, fontSize: 14)),
-                        ],
-                      )
-                    ],
+                  elevation: 6,
+                  margin: EdgeInsets.all(6),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.amber,
+                        child: Padding(
+                            padding: EdgeInsets.all(6),
+                            child: FittedBox(
+                                child: Text(
+                              "\$${tx.amount}",
+                              style: TextStyle(color: Colors.black),
+                            )))),
+                    title: Text(tx.title,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold)),
+                    subtitle: Text(
+                      DateFormat.yMMMMd("en_US").format(tx.date),
+                    ),
                   ),
                 );
+                // return Card(
+                //   child: Row(
+                //     children: [
+                //       Container(
+                //         constraints: BoxConstraints(minWidth: 88, maxWidth: 88),
+                //         padding: const EdgeInsets.all(10),
+                //         margin: const EdgeInsets.symmetric(
+                //             vertical: 10, horizontal: 15),
+                //         decoration: BoxDecoration(
+                //             border: Border.all(
+                //           color: Theme.of(context).colorScheme.primary,
+                //           width: 2,
+                //         )),
+                //         child: Text(
+                //           '\$' "${tx.amount.toStringAsFixed(2)}",
+                //           style: TextStyle(
+                //               fontWeight: FontWeight.bold,
+                //               fontSize: 20,
+                //               color: Colors.black),
+                //         ),
+                //       ),
+                //       Column(
+                //         crossAxisAlignment: CrossAxisAlignment.start,
+                //         children: [
+                //           Text(tx.title,
+                //               style: const TextStyle(
+                //                   fontSize: 18, fontWeight: FontWeight.bold)),
+                //           Text(DateFormat.yMMMMd("en_US").format(tx.date),
+                //               style: const TextStyle(
+                //                   color: Colors.grey, fontSize: 14)),
+                //         ],
+                //       )
+                //     ],
+                //   ),
+                // );
               }).toList(),
             ),
     );
